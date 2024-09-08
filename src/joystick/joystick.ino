f2414c3988c5829e1443b54b7a086fd7a6499158
AntonSangho <sanghoemail@gmail.com>
2024-09-08 22:48:18 +0900

    add code

---
+++ src/joystick/joystick.ino
@@ -0,0 +1,13 @@
+#include <Joystick.h>
+
+Joystick joystick(A0, A1, 13);
+
+void setup() {
+	Serial.begin(9600);
+}
+
+void loop() {
+	joystick.print("X: ", "\tY: ", "\tZ: ", 0, 100);
+	Serial.println();
+	delay(100);
+}
